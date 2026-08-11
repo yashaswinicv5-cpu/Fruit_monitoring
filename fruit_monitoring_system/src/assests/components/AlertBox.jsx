f3b0bd360f46d8ca528d6197d46acc95e4dd5127
Darshini c v <yashaswinicv5@gmail.com>
2026-08-11 19:45:25 +0530

    reverted

--- fruit_monitoring_system/src/assests/components/AlertBox.jsx
+++ fruit_monitoring_system/src/assests/components/AlertBox.jsx
@@ -1,6 +1,6 @@
 export default function AlertBox({ alert }) {
   return (
-    <div className="bg-red-950 border border-red-700 rounded-2xl p">
+    <div className="bg-red-950 border border-red-700 rounded-2xl p-4">
       {alert}
     </div>
   );
